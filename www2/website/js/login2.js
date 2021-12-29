51d45b39c949e0e734aa50cd3b8a32ab6691e4ec
Trần Bảo Kha <72544318+KL-Kha@users.noreply.github.com>
2021-12-29 18:01:55 +0700

    Delete www2 directory

--- www2/website/js/login2.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", "home2.php", true);
-    xmlhttp.send();
-    $("#logout").click(function() {
-        location.href = "logout2.php";
-    });
-});
